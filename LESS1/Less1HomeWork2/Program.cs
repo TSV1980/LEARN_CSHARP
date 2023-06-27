554476c010524e596500e0cadf89c44c59e3a2f8
TSV <sergey.v.t.33@gmail.com>
2023-06-27 23:12:36 +0300

    LESS 1 TASK2

--- LESS1/Less1HomeWork2/Program.cs
+++ LESS1/Less1HomeWork2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.Write("Введите три числа черел пробел:");
+string nums = Console.ReadLine();
+
+
+if (!string.IsNullOrEmpty(nums) && nums.Length > 0)
+{
+    string[] intnums = nums.Split(' ');
+    int max = int.Parse(intnums[0]);
+    for (int i = 1;i < intnums.Length; i++)
+    {
+        if (int.TryParse(intnums[i], out int currnum))
+            if (currnum > max)
+                max = currnum;
+    }
+    Console.Write("Максимальное число равно - " + max);
+}
