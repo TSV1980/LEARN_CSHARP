196e27ed17c95abf2e7fbad89915e47f1c05e0bc
TSV <sergey.v.t.33@gmail.com>
2023-06-27 22:54:07 +0300

    LESS 1 TASK1

--- LESS1/Less1HomeWork1/Program.cs
+++ LESS1/Less1HomeWork1/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.Write("Введите первое число:");
+string? num1str = Console.ReadLine();
+Console.Write("Введите второе число:");
+string? num2str = Console.ReadLine();
+
+int num1, num2 = 0;
+if (int.TryParse(num1str, out num1) && int.TryParse(num2str,out num2))
+{
+    if (num1 > num2)
+        Console.WriteLine($"Первое число {num1} больше второго числа {num2}");
+    else if (num1 < num2)
+        Console.WriteLine($"Первое число {num1} меньше второго числа {num2}");        
+    else if (num1 == num2)
+        Console.WriteLine($"Первое число {num1} равно второму числу {num2}");     
+}
+
+
+
 
