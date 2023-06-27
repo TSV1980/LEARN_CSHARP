31052fc682f1588bcc99ce096192eac2a78bdbb0
TSV <sergey.v.t.33@gmail.com>
2023-06-27 23:46:41 +0300

    LESS 1 TASK4

---
+++ LESS1/Less1Homework4/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.Write("Введите число N:");
+string? num1str = Console.ReadLine();
+
+int num1 = 0;
+if (int.TryParse(num1str, out num1))
+{
+    if (num1 >= 2)
+    {
+        for (int i = 2; i <= num1; i+=2)
+        {
+            Console.Write($" {i}");
+        }
+    }
+}
