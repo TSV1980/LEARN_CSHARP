aa1e1ce92edaf5c3db9926eb343c61baaba63b5c
TSV <sergey.v.t.33@gmail.com>
2023-07-05 23:06:18 +0300

    LESS 3

---
+++ LESS3/Less3HomeWork3/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+
+*/
+Console.Write("Введите число:");
+string? num1str = Console.ReadLine();
+if (int.TryParse(num1str, out int num2))
+    Console.WriteLine($"Последовательность кубов для числа {num2} = {GetCubes(num2)}");
+
+
+
+static string GetCubes(int num)
+{
+    string res = "";
+    for (int i = 1; i <= num; i++)
+    {
+        res += Math.Pow(i, 3);
+        if (i != num)
+            res += ", ";
+    }
+    return res;
+}
