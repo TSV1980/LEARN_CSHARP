54492e3df8279698ba408c30ec2e7a5e6c817945
TSV <sergey.v.t.33@gmail.com>
2023-07-06 23:37:11 +0300

    LESS 4

---
+++ LESS4/Less4HomeWork1/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+bool workAlways = true;
+
+while (workAlways)
+{
+    Console.WriteLine("Введите два целых числа через пробел: A и B. Или нажмите Q для выхода.");
+    string str = Console.ReadLine();
+    if (!string.IsNullOrEmpty(str))
+    {
+        if (str != "Q")
+        {
+            string[] res = str.Split();
+            if (res.Length == 2 && int.TryParse(res[0], out int A) && int.TryParse(res[1], out int B))
+            {
+                Console.WriteLine($"Возводим {A} в степень {B}. Результат = {Math.Pow(A,B)}");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Работа программы завершена.");
+            workAlways = false;
+            break;
+        }
+    } 
+}
