b18a60fc939e7da445bd2ee774b3e2b2c9587390
TSV <sergey.v.t.33@gmail.com>
2023-07-18 00:31:15 +0300

    LESS 5

---
+++ LESS6/Less6HomeWrok1/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+int EnterNumOfDigits()
+{
+    Console.WriteLine("Введите количество чисел:");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int[] EnterDigits(int num)
+{
+    int[] digits = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        Console.WriteLine($"Введите число №{i+1}:");
+        digits[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return digits;
+}
+
+int FindNumOfPositive(int[] digits)
+{
+    int posnum = 0;
+    for (int i = 0;i < digits.Length;i++)
+    {
+        if (digits[i] > 0) 
+            posnum ++;
+    }
+    return posnum;
+}
+
+int M = EnterNumOfDigits();
+int[] NUMS = EnterDigits(M);
+Console.WriteLine($"Число положительных чисел = {FindNumOfPositive(NUMS)}");
---
+++ LESS6/Less6HomeWrok2/Program.cs
@@ -0,0 +1,86 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+
+Console.WriteLine($"Задание линии №1");
+Line L1 = new Line();
+L1.EnterValues();
+Console.WriteLine($"Линия №1 -> {L1.ToString()}");
+
+Console.WriteLine($"Задание линии №2");
+Line L2 = new Line();
+L2.EnterValues();
+Console.WriteLine($"Линия №2 -> {L2.ToString()}");
+
+Intersection intersection = new Intersection();
+intersection.L1 = L1;
+intersection.L2 = L2;   
+intersection.FindIntersectionPoint();
+
+
+
+
+
+
+public class Line
+{
+    /// <summary>
+    /// Коэффициент К
+    /// </summary>
+    /// <value></value>
+    public int K { get; set;}
+
+    /// <summary>
+    /// Коэффициент B
+    /// </summary>
+    /// <value></value>
+    public int B { get; set;}
+
+    public Line(int K, int B)
+    {
+        this.K = K;
+        this.B = B;
+    }
+
+    public Line()
+    {
+        this.K = 1;
+        this.B = 1;
+    }
+
+
+    public void EnterValues()
+    {
+        Console.WriteLine("Введите коэффициент К:");
+        this.K = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите коэффициент B:");
+        this.K = Convert.ToInt32(Console.ReadLine());
+    }
+
+    public override string ToString()
+    {
+        return $"Y = {K}x + {B}";
+    }
+}
+
+
+
+public class Intersection
+{
+    public Line L1 { get; set; }
+
+    public Line L2 { get; set;} 
+
+    public void FindIntersectionPoint()
+    {
+        float X = (L1.B - L2.B)/(L2.K - L1.K);
+        float Y = (L1.B * X) + L1.B;
+
+        Console.WriteLine($"Точка пересечени - ({X},{Y}).");
+    }
+}
+
