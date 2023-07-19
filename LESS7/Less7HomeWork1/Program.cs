19c478ce051c9e0070cf21bc032052c15049a296
TSV <sergey.v.t.33@gmail.com>
2023-07-19 11:31:08 +0300

    LESS 5

---
+++ LESS7/Less7HomeWork1/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+Random rnd = new Random();
+//float N = 
+
+Console.WriteLine("Введите количество рядов:");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество колонок:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+float[,] MATRIX = new float[M, N];
+
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < M; j++)
+    {
+        MATRIX[i, j] = rnd.NextSingle() * 10.0f;
+        Console.Write($"{MATRIX[i, j]:F2}");
+        Console.Write($"   ");
+    }
+    Console.WriteLine();
+}
---
+++ LESS7/Less7HomeWork2/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+
+Console.WriteLine("Введите количество рядов:");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество колонок:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+float[,] matrix = GenerateMatrix(M, N);
+
+Console.WriteLine("Введите номер элемента для поиска:");
+int postofind = Convert.ToInt32(Console.ReadLine());
+
+ReturnElement(postofind, matrix);
+
+
+
+float[,] GenerateMatrix(int M, int N)
+{
+    
+    Random rnd = new Random();
+    //float N = 
+
+
+
+    float[,] matrix = new float[M, N];
+
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            matrix[i, j] = rnd.NextSingle() * 10.0f;
+            Console.Write($"{matrix[i, j]:F2}");
+            Console.Write($"   ");
+        }
+        Console.WriteLine();
+    }
+
+    return matrix;
+}
+
+float ReturnElement(int pos, float[,] matrix)
+{
+    int row = matrix.GetLength(0);
+    int col = matrix.GetLength(1);
+
+    if (pos < 0 || pos > row * col)
+    {
+        Console.WriteLine($"В массиве нет данного элемента");
+        return 0;
+    }
+
+    pos--;
+    
+    int posinrow = pos % col;
+
+    float el = matrix[pos / col, posinrow];
+
+    Console.WriteLine($"Найденный элемент = {el:F2}");
+
+    return el;
+}
